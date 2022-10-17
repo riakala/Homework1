942f9e06c75a571bd416d6ba0ba7f4def631e805
Ria Kala <105014704+riakala@users.noreply.github.com>
2022-10-17 19:20:00 -0400

    Create test.java

---
+++ test.java
@@ -0,0 +1,8 @@
+package hw1;
+
+public class test {
+	public static void main(String[] args) {
+		student Ria = new student(75, 88, 66, 96);
+		System.out.println("Your average is " + Ria.calcAvg(75, 88, 66, 96));
+	}
+}
